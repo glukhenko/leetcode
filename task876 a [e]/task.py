c05a72c793b4ffc1d211b9c21cdfaf5cf47d10a7
glukhenko <anton.glukhenko@gmail.com>
2021-12-05 01:48:35 +0300

    add 876 solution

--- task876 a [e]/task.py
+++ task876 a [e]/task.py
@@ -1,11 +1,14 @@
 # https://leetcode.com/problems/middle-of-the-linked-list/submissions/
 
+from typing import Optional, List
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """
