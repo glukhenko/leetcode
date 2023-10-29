4b11aa6de1430a8b730d4ede27f49c7f750ac598
Glukhenko <anton.glukhenko@gmail.com>
2023-10-29 22:56:34 +0300

    add 3 solution

--- task3 a [m]/task.py
+++ task3 a [m]/task.py
@@ -1,25 +1,39 @@
-# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+from typing import List, Optional
 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Суть решения: использовать 2 указателя
+        Первый указатель - говорит где начинается подстрока
+        Второй указатель - ищет дубли
+        Как дубль найден, надо:
+        - расчитать размер подстроки
+        - переместить первый указатель, пока не удалится дубль и удалить все символы по пути
+        """
+        visited = set()
+        slow, fast = 0, 0
+        max_sub = float('-inf')
 
-        count = 0
-        chars = []
+        while fast < len(s):
+            if s[fast] in visited:
+                # расчитаем максимум
+                max_sub = max(max_sub, len(visited))
+                # сместим указатель начало подстроки
+                while s[fast] in visited:
+                    visited.remove(s[slow])
+                    slow += 1
 
-        for char in s:
-            if char in chars:
-                count = max(count, len(chars))
-                index = chars.index(char)
-                chars = chars[index + 1:]
-            chars.append(char)
-        count = max(count, len(chars))
-        return count
+            visited.add(s[fast])
+            fast += 1
 
+        return max(max_sub, len(visited))
 
 
 if __name__ == '__main__':
     s = "abcabcbb"
-    expected = 3
     result = Solution().lengthOfLongestSubstring(s)
-    print(f'result: {result}')
+    expected = 3
+    assert result == expected, f'Bad result: {result}, but expected: {expected}'
--- task3 a [m]/test.py
+++ task3 a [m]/test.py
@@ -1,4 +1,6 @@
 import unittest
+from typing import List
+
 from task import Solution
 
 
@@ -6,45 +8,27 @@ class TestTask(unittest.TestCase):
 
     def test_case1(self):
         s = "abcabcbb"
+        result = Solution().lengthOfLongestSubstring(s)
         expected = 3
-        self.assertEqual(Solution().lengthOfLongestSubstring(s), expected,
-                         f'incorrect result for s: {s}, expected: {expected}')
+        assert result == expected, f'Bad result: {result}, but expected: {expected}'
 
     def test_case2(self):
         s = "bbbbb"
+        result = Solution().lengthOfLongestSubstring(s)
         expected = 1
-        self.assertEqual(Solution().lengthOfLongestSubstring(s), expected,
-                         f'incorrect result for s: {s}, expected: {expected}')
+        assert result == expected, f'Bad result: {result}, but expected: {expected}'
 
     def test_case3(self):
         s = "pwwkew"
+        result = Solution().lengthOfLongestSubstring(s)
         expected = 3
-        self.assertEqual(Solution().lengthOfLongestSubstring(s), expected,
-                         f'incorrect result for s: {s}, expected: {expected}')
+        assert result == expected, f'Bad result: {result}, but expected: {expected}'
 
     def test_case4(self):
-        s = ""
-        expected = 0
-        self.assertEqual(Solution().lengthOfLongestSubstring(s), expected,
-                         f'incorrect result for s: {s}, expected: {expected}')
-
-    def test_case5(self):
-        s = " "
-        expected = 1
-        self.assertEqual(Solution().lengthOfLongestSubstring(s), expected,
-                         f'incorrect result for s: {s}, expected: {expected}')
-
-    def test_case6(self):
         s = "dvdf"
+        result = Solution().lengthOfLongestSubstring(s)
         expected = 3
-        self.assertEqual(Solution().lengthOfLongestSubstring(s), expected,
-                         f'incorrect result for s: {s}, expected: {expected}')
-
-    def test_case7(self):
-        s = "tmmzuxt"
-        expected = 5
-        self.assertEqual(Solution().lengthOfLongestSubstring(s), expected,
-                         f'incorrect result for s: {s}, expected: {expected}')
+        assert result == expected, f'Bad result: {result}, but expected: {expected}'
 
 
 if __name__ == '__main__':
